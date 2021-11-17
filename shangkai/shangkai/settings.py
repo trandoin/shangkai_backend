bfd3420a23646bde0b113313bee750ca6d3c807d
Govind Suman <govindsuman118@gmail.com>
2021-11-17 12:14:46 +0530

    database config updated

--- shangkai/shangkai/settings.py
+++ shangkai/shangkai/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zzjfseaw#khg4nq$%j*&9z=y$lntt_wzd4y3$i^!*=!#un3d3!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'shangkai.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shangkaidb',
+        'USER':'shangkai-db',
+        'PASSWORD':'shangkaipass-travel',
+        'HOST':'shangkai-identifier.cog4jorwkti6.ap-south-1.rds.amazonaws.com',
+        'PORT':'3306',
     }
 }
 
