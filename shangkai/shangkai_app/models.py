ac63af70f9c45384500ad93b41782619604631cf
Trando_tech <68798607+trandoin@users.noreply.github.com>
2021-11-20 12:40:55 +0530

    timezone error fixed

--- shangkai/shangkai_app/models.py
+++ shangkai/shangkai_app/models.py
@@ -6,7 +6,7 @@ from users.models import (
 )
 
 class About_Us(models.Model):
-    datetime = models.DateTimeField("Created At", auto_now_add=True,default=timezone.now)
+    datetime = models.DateTimeField("Created At", auto_now_add=True)
     about_us = models.TextField("about_us", null=True, max_length=1000)
     status = models.CharField("status", null=True, default="0", max_length=255)
 
@@ -17,7 +17,7 @@ class About_Us(models.Model):
         )
 
 class Footer_Copyright(models.Model):
-    datetime = models.DateTimeField("Created At", auto_now_add=True,default=timezone.now)
+    datetime = models.DateTimeField("Created At", auto_now_add=True)
     footer = models.TextField("footer", null=True, max_length=1000)
     status = models.CharField("status", null=True, default="0", max_length=255)
 
@@ -27,7 +27,7 @@ class Footer_Copyright(models.Model):
             "Footer",
         )    
 class Hotspot_Category(models.Model):
-    datetime = models.DateTimeField("Created At", auto_now_add=True,default=timezone.now)
+    datetime = models.DateTimeField("Created At", auto_now_add=True)
     title = models.CharField("title", null=True, max_length=1000)
     status = models.CharField("status", null=True, default="0", max_length=255)
 
@@ -38,7 +38,7 @@ class Hotspot_Category(models.Model):
         )    
 
 class Hot_Spots(models.Model):
-    datetime = models.DateTimeField("Created At", auto_now_add=True,default=timezone.now)
+    datetime = models.DateTimeField("Created At", auto_now_add=True)
     title = models.CharField("title", null=True,  max_length=255)
     sub_title = models.CharField("sub_title", null=True,  max_length=255)
     city = models.CharField("city", null=True,  max_length=255)
@@ -65,7 +65,7 @@ class Hot_Spots(models.Model):
         )           
 
 class Comments_All(models.Model):
-    datetime = models.DateTimeField("Created At", auto_now_add=True,default=timezone.now)
+    datetime = models.DateTimeField("Created At", auto_now_add=True)
     user = models.ForeignKey("users.Normal_UserReg",on_delete=models.CASCADE,default=None)
     post_id = models.CharField("post_id", null=True, max_length=255)
     comments = models.CharField("comments", null=True, max_length=2000)
@@ -81,7 +81,7 @@ class Comments_All(models.Model):
 # payment transaction
 
 class Payment_Transaction(models.Model):
-    datetime = models.DateTimeField("Created At", auto_now_add=True,default=timezone.now)
+    datetime = models.DateTimeField("Created At", auto_now_add=True)
     user_id = models.CharField("user_id", null=True, max_length=255)
     transaction_id = models.CharField("transaction_id", null=True, max_length=255)
     transaction_type_id = models.CharField("transaction_type_id", null=True, max_length=2000)
