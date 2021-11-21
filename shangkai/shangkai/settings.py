ed07fadd2455931b681b77a10d61445f497ea39d
Trando_tech <68798607+trandoin@users.noreply.github.com>
2021-11-21 11:08:11 +0530

    STATICFILES_STORAGE removed

--- shangkai/shangkai/settings.py
+++ shangkai/shangkai/settings.py
@@ -138,7 +138,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR,'static')
 STATIC_URL = '/static/'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ###### AWS S3 ####
