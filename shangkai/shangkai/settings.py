bb8f3985981229c0c67d070d30e4c181e3eb36f9
Trando_tech <68798607+trandoin@users.noreply.github.com>
2021-11-21 11:22:36 +0530

    STATICFILES_STORAGE added

--- shangkai/shangkai/settings.py
+++ shangkai/shangkai/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
-    "django-storages",
+    "storages",
     "users",
     "clients",
     "shangkai_app",
@@ -139,7 +139,8 @@ STATIC_ROOT = os.path.join(PROJECT_DIR,'static')
 STATIC_URL = '/static/'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 
 ###### AWS S3 ####
