42a3b87b4872dc98436d50fb476e89a7c6e13c79
Trando_tech <68798607+trandoin@users.noreply.github.com>
2021-11-21 11:20:13 +0530

    django-storages added

--- shangkai/shangkai/settings.py
+++ shangkai/shangkai/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
-    "storages",
+    "django-storages",
     "users",
     "clients",
     "shangkai_app",
