fb6b22dd53d5c8787a33d2d2c027e403a9dfa02b
Trando_tech <68798607+trandoin@users.noreply.github.com>
2021-11-21 11:12:25 +0530

    error fixing

--- shangkai/shangkai/settings.py
+++ shangkai/shangkai/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    "storages",
     "users",
     "clients",
     "shangkai_app",
@@ -138,7 +139,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR,'static')
 STATIC_URL = '/static/'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ###### AWS S3 ####
