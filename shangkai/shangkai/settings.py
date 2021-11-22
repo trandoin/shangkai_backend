ecff9a277ca77ba52196aaae836419ff73de7341
Trando_tech <68798607+trandoin@users.noreply.github.com>
2021-11-22 16:16:27 +0530

    error fixing

--- shangkai/shangkai/settings.py
+++ shangkai/shangkai/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
-    "storage",
+    "storages",
     "users",
     "clients",
     "shangkai_app",
