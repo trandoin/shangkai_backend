0e4679ef08f079d73030ec402a753dda06c06568
Trando_tech <68798607+trandoin@users.noreply.github.com>
2021-11-22 19:20:10 +0530

    AccounDetailsBookingViewSet create api

--- shangkai/users/views.py
+++ shangkai/users/views.py
@@ -68,4 +68,41 @@ class AccounDetailsBookingViewSet(viewsets.ViewSet):
                 {"message": "Sorry No data found !"},
                 status=status.HTTP_400_BAD_REQUEST,
             )
-        return Response(hotel_data_dic.data, status=status.HTTP_200_OK)                        
+        return Response(hotel_data_dic.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+
+        user_id = request.POST.get("user_id", None)
+        acc_holder = request.POST.get("acc_holder", None)
+        account_no = request.POST.get("account_no", None)
+        bannk_name = request.POST.get("bannk_name", None)
+        bank_branch = request.POST.get("bank_branch", None)
+        ifsc_code = request.POST.get("ifsc_code", None)
+        bank_state = request.POST.get("bank_state", None)
+        pan_card = request.POST.get("pan_card", None)
+
+        try:
+            user_inst = Normal_UserReg.objects.get(id=user_id)
+        except:
+
+            return Response(
+                {"message": "No user found !"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        users_inst = User_Account_Details.objects.create(
+            user_id=user_id,
+            acc_holder=acc_holder,
+            account_no=account_no,
+            bannk_name=bannk_name,
+            bank_branch=bank_branch,
+            ifsc_code=ifsc_code,
+            bank_state=bank_state,
+            pan_card=pan_card,
+        )
+        users_inst.save()
+
+        users_data = serializers.AccountDetailsBookingSerializer(
+            User_Account_Details.objects.filter(id=users_inst.id), many=True
+        )
+        return Response(users_data.data[0], status=status.HTTP_200_OK)                            
