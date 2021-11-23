c0cf1c0c5ea3f4e73ec6e0ab34c55413ce14276a
Trando_tech <68798607+trandoin@users.noreply.github.com>
2021-11-24 00:39:58 +0530

    error5 fixing

--- shangkai/clients/serializers.py
+++ shangkai/clients/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from .models import (
-    Reg_Hotel,
-    Room_Register,
+    # Reg_Hotel,
+    # Room_Register,
     User_Register,
     Driver_Reg,
     Cabs_Reg,
@@ -14,16 +14,16 @@ class UserRegisterSerializer(ModelSerializer):
         fields = "__all__"
 
 
-class HotelRegisterSerializer(ModelSerializer):
-    class Meta:
-        model = Reg_Hotel
-        fields = "__all__"
+# class HotelRegisterSerializer(ModelSerializer):
+#     class Meta:
+#         model = Reg_Hotel
+#         fields = "__all__"
 
 
-class RoomRegisterSerializer(ModelSerializer):
-    class Meta:
-        model = Room_Register
-        fields = "__all__"
+# class RoomRegisterSerializer(ModelSerializer):
+#     class Meta:
+#         model = Room_Register
+#         fields = "__all__"
 
 class DriverRegisterSerializer(ModelSerializer):
     class Meta:
