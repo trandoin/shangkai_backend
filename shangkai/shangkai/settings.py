fae3dcdc97b6b3c021ecc12307cbb988534cfe02
Govind Suman <govindsuman118@gmail.com>
2021-11-27 12:25:58 +0530

    auth_travel app installed

--- shangkai/shangkai/settings.py
+++ shangkai/shangkai/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
-    # "storages",
+    "auth_travel",
     "corsheaders",
     "users",
     "clients",
