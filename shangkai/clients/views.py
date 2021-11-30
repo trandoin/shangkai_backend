61459cf33c3192f3700d99e784e9617b0269a9de
Govind S <61695548+SDnivog@users.noreply.github.com>
2021-11-30 12:02:05 +0530

    hotel_cat error fixed

--- shangkai/clients/views.py
+++ shangkai/clients/views.py
@@ -134,7 +134,7 @@ class HotelRegistrationViewSet(viewsets.ViewSet):
 
         user_id = request.POST.get("user_id", None)
         hotel_code = request.POST.get("hotel_code", None)
-        # hotel_cat = request.POST.get("hotel_cat", None)
+        hotel_cat = request.POST.get("hotel_cat", None)
         hotel_name = request.POST.get("hotel_name", None)
         hotel_address = request.POST.get("hotel_address", None)
         hotel_city = request.POST.get("hotel_city", None)
@@ -148,7 +148,7 @@ class HotelRegistrationViewSet(viewsets.ViewSet):
 
         try:
             user_inst = User_Register.objects.get(id=user_id)
-            # hotel_cat_inst = Hotel_Category.objects.get(id=hotel_cat)
+            hotel_cat_inst = Hotel_Category.objects.get(id=hotel_cat)
 
         except:
 
@@ -158,7 +158,7 @@ class HotelRegistrationViewSet(viewsets.ViewSet):
             )
         users_inst = Reg_Hotel.objects.create(
             user=user_inst,
-            # hotel_cat=hotel_cat_inst,
+            hotel_cat=hotel_cat_inst,
             hotel_code=hotel_code,
             hotel_name=hotel_name,
             hotel_address=hotel_address,
