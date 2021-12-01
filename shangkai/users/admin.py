8501182669a7c6012653476509f68fede5c57ba8
Govind S <61695548+SDnivog@users.noreply.github.com>
2021-12-01 16:30:47 +0530

    User_Hotel_Cart Registered at admin

--- shangkai/users/admin.py
+++ shangkai/users/admin.py
@@ -7,6 +7,7 @@ from .models import (
     User_Cab_Booking,
     User_Hotel_Booking,
     Normal_UserReg,
+    User_Hotel_Cart,
 )
 
 # Register your models here.
@@ -32,6 +33,12 @@ class BookingHotel(admin.ModelAdmin):
 
 
 admin.site.register(User_Hotel_Booking, BookingHotel)
+
+class HotelCart(admin.ModelAdmin):
+    list_display = ["id","user","hotel_id","check_in_date","check_in_time","check_out_date","check_out_time","guest_no","rooms","amount_booking"]
+
+
+admin.site.register(User_Hotel_Cart, HotelCart)
 
 class CabBooking(admin.ModelAdmin):
     list_display = ["id","user","user_ip","car_id","driver_id","cab_bookid","check_in_date","check_in_time","check_out_date","check_out_time","start_from","end_trip","distance","amount_booking","no_guests"]
