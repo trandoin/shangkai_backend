49ff55cecbf6e813904bb0aabc2139457e5d5f52
Govind S <61695548+SDnivog@users.noreply.github.com>
2021-12-06 02:31:07 +0530

    Trip cart status changed

--- shangkai/users/views.py
+++ shangkai/users/views.py
@@ -924,11 +924,11 @@ class UserTripsCartViewSet(viewsets.ViewSet):
             return Response({"message": "Details not found"}, status=status.HTTP_200_OK)
 
     def update(self, request, pk=None):
-        # user_id = request.GET.get("user_id", None)
+        user_id = request.GET.get("user_id", None)
         cart_id = request.GET.get("cart_id", None)
         trip_cart_status = request.GET.get("cart_status", None)
 
-        if pk is None:
+        if pk is None and user_id is None:
             return Response(
                 {"message": "Invalid Input"}, status=status.HTTP_400_BAD_REQUEST
             )
