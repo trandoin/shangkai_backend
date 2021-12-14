350957614effb75f6b5c365252b6a15890708a3c
Govind S <61695548+SDnivog@users.noreply.github.com>
2021-12-14 16:23:34 +0530

    error fixing

--- shangkai/users/views.py
+++ shangkai/users/views.py
@@ -60,36 +60,32 @@ class UserRegisterViewSet(viewsets.ViewSet):
         mobile = request.POST.get("mobile", None)
         password = request.POST.get("password", None)
         image = request.POST.get("image", None)
+        
         try:
-            sm_users = Normal_UserReg.objects.filter(email=email)
-            users_data_dic = serializers.NormalUserRegisterSerializer(
-                sm_users, many=True
-            )
-            if email in users_data_dic:
-                    return Response(
-                    {"message": "Email id already exists !"},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )    
+            sm_users = Normal_UserReg.objects.all(email !=email)
             users_inst = Normal_UserReg.objects.create(
-                user_id=user_id,
-                user_ip=user_ip,
-                name=name,
-                email=email,
-                mobile=mobile,
-                password=password,
-                image=image,
-            )
-            users_inst.save()
+            user_id=user_id,
+            user_ip=user_ip,
+            name=name,
+            email=email,
+            mobile=mobile,
+            password=password,
+            image=image,
+        )
+        users_inst.save()
 
-            users_data = serializers.NormalUserRegisterSerializer(
-                Normal_UserReg.objects.filter(id=users_inst.id), many=True
-            )
-            return Response(users_data.data[0], status=status.HTTP_200_OK)
+        users_data = serializers.NormalUserRegisterSerializer(
+            Normal_UserReg.objects.filter(id=users_inst.id), many=True
+        )
+        return Response(users_data.data[0], status=status.HTTP_200_OK)
+
         except:
             return Response(
-                {"message": "Sorry!! something went wrong"},
+                {"message": "Email id already exists !"},
                 status=status.HTTP_400_BAD_REQUEST,
             )
+        
+
     def update(self, request, pk=None):
         user_id = request.POST.get("user_id", None)
         name = request.POST.get("name", None)
