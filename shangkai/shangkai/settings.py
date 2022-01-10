ee54dcca8842c3bc828d1d547077b99929f5af73
Trando_tech <68798607+trandoin@users.noreply.github.com>
2022-01-10 10:04:57 +0530

    database host chaged

--- shangkai/shangkai/settings.py
+++ shangkai/shangkai/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         "NAME": "shangkai_shangkai_db",
         "USER": "shangkai_shangkaiUser466",
         "PASSWORD": "sZrGWzBPpU2t@pA",
-        "HOST": "localhost",
+        "HOST": "127.0.0.1",
         "PORT": "3306",
     }
 }
