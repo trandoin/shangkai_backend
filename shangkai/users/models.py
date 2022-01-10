37b64cffd1b8c7860597393b6c7930f909b11e2b
Govind S <61695548+SDnivog@users.noreply.github.com>
2022-01-10 14:13:11 +0530

    datefield default value updated

--- shangkai/users/models.py
+++ shangkai/users/models.py
@@ -415,7 +415,7 @@ class User_Guide_Booking(models.Model):
         db_constraint=False,
     )
     no_guests = models.CharField("no_guests", null=True, max_length=255)
-    booking_date = models.DateField("booking_date", null=True, max_length=255)
+    booking_date = models.DateField("booking_date",default="2021-03-09", null=True, max_length=255)
     guide_amount = models.CharField("guide_ammount", null=True, max_length=255)
     razorpay_id = models.CharField(
         "razorpay_id", null=True, default="0", max_length=255
@@ -444,7 +444,7 @@ class User_Hotspots_Cart(models.Model):
         db_constraint=False,
     )
     no_guests = models.CharField("no_guests", null=True, max_length=255)
-    booking_date = models.DateField("booking_date", null=True, max_length=255)
+    booking_date = models.DateField("booking_date",default="2021-03-09", null=True, max_length=255)
     cart_amount = models.CharField("cart_ammount", null=True, max_length=255)
     status = models.CharField("status", null=True, default="0", max_length=255)
 
@@ -470,7 +470,7 @@ class User_Hotspots_Bookings(models.Model):
         db_constraint=False,
     )
     no_guests = models.CharField("no_guests", null=True, max_length=255)
-    booking_date = models.DateField("booking_date", null=True, max_length=255)
+    booking_date = models.DateField("booking_date",default="2021-03-09", null=True, max_length=255)
     booking_amount = models.CharField("booking_amount", null=True, max_length=255)
     razorpay_id = models.CharField(
         "razorpay_id", null=True, default="0", max_length=255
