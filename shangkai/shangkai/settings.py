735bae27c02a52a412dd3e224dd68b6465966ddc
Govind S <61695548+SDnivog@users.noreply.github.com>
2022-01-11 15:54:15 +0530

    static url changed

--- shangkai/shangkai/settings.py
+++ shangkai/shangkai/settings.py
@@ -145,7 +145,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/root/shangkai_env/shangkai_backend/shangkai/static/'
+STATIC_URL = '/static/'
+# STATIC_URL = '/root/shangkai_env/shangkai_backend/shangkai/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
