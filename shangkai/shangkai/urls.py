7c6a5de95615fec80ef46727ebdf5032c58a72a3
Govind S <61695548+SDnivog@users.noreply.github.com>
2022-03-10 16:37:20 +0530

    shangkai_app -> url changed

--- shangkai/shangkai/urls.py
+++ shangkai/shangkai/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path("", include("shangkai_app.urls")),
+    path("api/", include("shangkai_app.urls")),
     path("users/", include("users.urls")),
     path("clients/", include("clients.urls")),
     path("auth_clients/", include("auth_travel.urls")),
