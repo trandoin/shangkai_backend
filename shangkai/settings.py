0e51bde3560cc7b3ed49fb97f099d36fe2d0e11d
Govind S <61695548+SDnivog@users.noreply.github.com>
2022-07-13 11:18:04 +0530

    smtp cre updated

--- shangkai/settings.py
+++ shangkai/settings.py
@@ -157,8 +157,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.hostinger.in"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "shangkai-mail@trando.in"
-EMAIL_HOST_PASSWORD = "Shangkai@12#$"
+EMAIL_HOST_USER = "learning@trando.in"
+EMAIL_HOST_PASSWORD = "Learn#46#$(*"
 
 
 # STATIC_URL = '/root/shangkai_env/shangkai_backend/shangkai/static/'
